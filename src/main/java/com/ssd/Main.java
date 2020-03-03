3345b2b3268367d0c53da4c5fb90cc2d89ea8434
miguel090 <miguelaqcs@gmail.com>
2020-03-03 02:45:40 +0000

    added maven and bouncy castle created basic structure for program Does a very basic proof of work

---
+++ src/main/java/com/ssd/Main.java
@@ -0,0 +1,30 @@
+package com.ssd;
+
+import com.google.gson.GsonBuilder;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
+    public static int difficulty = 5;
+
+    public static void main(String[] args) {
+        //add our blocks to the blockchain ArrayList:
+        blockchain.add(new Block("Hi im the first block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
+
+        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+    }
+}
---
+++ src/main/java/com/ssd/utils.java
@@ -0,0 +1,22 @@
+package com.ssd;
+
+import org.bouncycastle.util.encoders.Hex;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class utils {
+    public static String getsha256(String input){
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        assert digest != null;
+        byte[] hash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+        String sha256hex = new String(Hex.encode(hash));
+        return sha256hex;
+    }
+}
