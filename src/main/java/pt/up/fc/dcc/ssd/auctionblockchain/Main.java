4b7e3b533a6f8e1d0c21b56b478f75999689b971
FredyR4zox <frederico.almeida@live.com.pt>
2020-06-05 18:43:20 +0100

    Update

--- src/main/java/pt/up/fc/dcc/ssd/auctionblockchain/Main.java
+++ src/main/java/pt/up/fc/dcc/ssd/auctionblockchain/Main.java
@@ -1,38 +1,71 @@
 package pt.up.fc.dcc.ssd.auctionblockchain;
 
+import pt.up.fc.dcc.ssd.auctionblockchain.Blockchain.Block;
 import pt.up.fc.dcc.ssd.auctionblockchain.Blockchain.BlockchainUtils;
-import pt.up.fc.dcc.ssd.auctionblockchain.Kademlia.KBucketManager;
-import pt.up.fc.dcc.ssd.auctionblockchain.Kademlia.KademliaNode;
-import pt.up.fc.dcc.ssd.auctionblockchain.Kademlia.KademliaServer;
-import pt.up.fc.dcc.ssd.auctionblockchain.Kademlia.KademliaUtils;
+import pt.up.fc.dcc.ssd.auctionblockchain.Blockchain.Transaction;
+import pt.up.fc.dcc.ssd.auctionblockchain.Client.Bid;
+import pt.up.fc.dcc.ssd.auctionblockchain.Kademlia.*;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.SecureRandom;
 import java.security.Security;
+import java.util.Random;
 
 
 public class Main {
-        public static void main(String[] args) throws InterruptedException, java.io.IOException {
-                Security.addProvider(new BouncyCastleProvider());
+    public static void main(String[] args) throws InterruptedException, java.io.IOException {
+        Security.addProvider(new BouncyCastleProvider());
 
-                String myIpAddress = KademliaUtils.getMyIpAddress();
-                if(myIpAddress == null){
-                        System.out.println("Error getting IP address");
-                        return;
-                }
-                System.out.println("My IP address is " + myIpAddress);
-
-                KademliaNode myNode = new KademliaNode(myIpAddress, KademliaUtils.bootstrapNodePort, KademliaUtils.bootstrapNodeID);
-                KBucketManager manager = new KBucketManager(myNode);
-
-                KademliaServer server = new KademliaServer(KademliaUtils.bootstrapNodePort, manager);
-                server.start();
-
-                Wallet creator = new Wallet();
-                System.out.println("Creator address: " + creator.getAddress());
-                System.out.println("Creator public key: " + creator.getPubKey().toString());
-                System.out.println("Creator private key: " + creator.getPrivKey().toString());
-
-                BlockchainUtils.createGenesisBlock(creator);
-
-                server.blockUntilShutdown();
+        String myIpAddress = KademliaUtils.getMyIpAddress();
+        if(myIpAddress == null){
+            System.out.println("Error getting IP address");
+            return;
         }
+        System.out.println("My IP address is " + myIpAddress);
+
+        KademliaNode myNode = new KademliaNode(myIpAddress, KademliaUtils.bootstrapNodePort, KademliaUtils.bootstrapNodeID);
+        KBucketManager manager = new KBucketManager(myNode);
+
+        KademliaServer server = new KademliaServer(KademliaUtils.bootstrapNodePort, manager);
+        server.start();
+
+        Wallet creator = new Wallet();
+        System.out.println("Creator address: " + creator.getAddress());
+        System.out.println("Creator public key: " + creator.getPubKey().toString());
+        System.out.println("Creator private key: " + creator.getPrivKey().toString());
+
+        KademliaClient client = new KademliaClient(manager);
+        BlockchainUtils.setMiner(creator);
+        BlockchainUtils.setKademliaClient(client);
+        BlockchainUtils.createGenesisBlock(creator);
+
+
+
+        byte[] rand = new byte[Utils.hashAlgorithmLengthInBytes];
+        Random random = new SecureRandom();
+        random.nextBytes(rand);
+
+        Wallet alice = Wallet.createWalletFromFile("alice");
+        Bid bid = new Bid(creator, new String(rand), alice.getAddress(), BlockchainUtils.minerReward/2, 5);
+        Transaction transaction = new Transaction(alice, bid);
+
+        BlockchainUtils.addTransaction(transaction);
+
+
+        random.nextBytes(rand);
+        Wallet bob = Wallet.createWalletFromFile("bob");
+        bid = new Bid(creator, new String(rand), bob.getAddress(), BlockchainUtils.minerReward/2, 5);
+        transaction = new Transaction(bob, bid);
+
+        BlockchainUtils.addTransaction(transaction);
+
+
+
+
+        Thread.sleep(10000);
+
+        System.out.println(BlockchainUtils.getLongestChain().makeJson());
+
+        server.blockUntilShutdown();
+    }
 }
